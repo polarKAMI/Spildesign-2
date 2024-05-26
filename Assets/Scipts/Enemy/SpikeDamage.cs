c7e69b237f9b550cbeb3de2dd8c4d0472778e1b7
EkstcDK <147029972+EkstcDK@users.noreply.github.com>
2024-05-26 14:45:44 +0200

    spikes dræber dig nu

--- Assets/Scipts/Enemy/SpikeDamage.cs
+++ Assets/Scipts/Enemy/SpikeDamage.cs
@@ -6,12 +6,13 @@ public class SpikeDamage : MonoBehaviour
 {
     public Health health;
 
-
-
-    public void OnCollisionEnter2D(Collision2D collision)
+    // This method will be called when another collider enters the trigger collider attached to the object where this script is attached
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.tag == "Player")
+        // Check if the object that entered the trigger has the tag "Player"
+        if (other.gameObject.CompareTag("Player"))
         {
+            // Access the Health component on the player and apply damage
             health.TakeDamage(10);
         }
     }
