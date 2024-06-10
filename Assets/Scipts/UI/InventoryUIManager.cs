bf8ba98f1a89c12aa0e65f2da3fc137d1ceef690
polarKAMI <145371945+polarKAMI@users.noreply.github.com>
2024-06-10 22:19:09 +0200

    inv fix skud ud far

--- Assets/Scipts/UI/InventoryUIManager.cs
+++ Assets/Scipts/UI/InventoryUIManager.cs
@@ -9,33 +9,37 @@ public class InventoryUIManager : MonoBehaviour
     public GameObject inventorySlotPrefab;
     public Transform inventoryPanel;
     public GameObject inventoryPanelObject;
-    public InventorySO inventorySO; // Reference to the InventorySO scriptable object
-
+    public InventorySO inventorySO;
 
     private List<GameObject> inventorySlots = new List<GameObject>(); // Keep track of inventory slots
 
     private bool isMoving = false;
-   
-    public GameObject emptyInventoryImage; // Reference to the empty inventory image
-    public bool isEmptyInventory = false; // Flag to track if the inventory is empty
+
+    public GameObject emptyInventoryImage;
+    public bool isEmptyInventory = false;
 
     public GameObject border;
 
-    public TMP_Text itemCounterText; // Reference to TextMeshPro component
-    public GameObject itemCounter; // Reference to ItemCounter GameObject
+    public TMP_Text itemCounterText;
+    public GameObject itemCounter;
 
     public bool isOpen = false;
-    private int currentIndex = -1;
+    public bool doLogSlots = false;
 
-    public GameObject ammoBar; // Reference to the AmmoBar UI GameObject
-    public GameObject ammoBarBoarder; // Reference to the AmmoBar UI GameObject
+    public GameObject ammoBar;
+    public GameObject ammoBarBoarder;
 
     public TMP_Text specTXT;
 
     private PlayerMovement playerMovement;
+
+    private int slotCount = 16;
+    private float haloScale = 50f;
+    private float startingAngle = 180f;
+
     private void Start()
     {
-        playerMovement = FindObjectOfType<PlayerMovement>(); // Find the PlayerMovement script in the scene
+        playerMovement = FindObjectOfType<PlayerMovement>();
         ammoBar.SetActive(false);
         ammoBarBoarder.SetActive(false);
         if (inventorySO != null)
@@ -43,14 +47,14 @@ public class InventoryUIManager : MonoBehaviour
             inventorySO.WipeInventory();
         }
     }
+
     public void OpenInventory()
     {
-
         if (playerMovement != null)
         {
             playerMovement.DisableMovement();
         }
-        // Update inventory UI
+
         UpdateInventoryUI();
         isOpen = true;
 
@@ -64,26 +68,20 @@ public class InventoryUIManager : MonoBehaviour
             ammoBarBoarder.SetActive(true);
         }
 
-        currentIndex = 0;
-
-        // Update specs text
         SetSpecs();
     }
 
     public void CloseInventory()
     {
-        // Close the options panel if it's active
         OptionsPanelManager optionsPanelManager = FindObjectOfType<OptionsPanelManager>();
         if (optionsPanelManager != null)
         {
             optionsPanelManager.ToggleOptionsPanel(false);
         }
 
-        // Close the inventory panel
         inventoryPanelObject.SetActive(false);
         ClearInventoryUI();
 
-        // Enable player movement when closing inventory
         if (playerMovement != null)
         {
             playerMovement.EnableMovement();
@@ -120,43 +118,28 @@ public class InventoryUIManager : MonoBehaviour
 
     public void UpdateInventoryUI()
     {
-        // Clear existing inventory UI
         ClearInventoryUI();
-
-        // Get the current inventory size
         int inventorySize = inventorySO.Size;
-
-        // Check if the inventory is empty
         if (inventorySize == 0)
         {
-            // Show the empty inventory image
             emptyInventoryImage.SetActive(true);
-
-
-            // Set the flag to indicate empty inventory
             isEmptyInventory = true;
-
-            // Disable options panel
             OptionsPanelManager optionsPanelManager = FindObjectOfType<OptionsPanelManager>();
+
             if (optionsPanelManager != null)
             {
                 optionsPanelManager.ToggleOptionsPanel(false);
             }
-            
+
             if (border != null)
             {
                 border.SetActive(false);
             }
-            // Return without creating inventory slots if the inventory is empty
-            return;
         }
         else
         {
             inventoryPanelObject.SetActive(true);
-            // Hide the empty inventory image
             emptyInventoryImage.SetActive(false);
-
-            // Reset the flag
             isEmptyInventory = false;
 
             if (border != null)
@@ -164,72 +147,40 @@ public class InventoryUIManager : MonoBehaviour
                 border.SetActive(true);
             }
 
-            // Always show 16 slots
-            int slotCount = 16;
             float angleStep = -360f / slotCount;
-            float radius = 100f; // Adjust as needed
-            float startingAngle = 180f; // Adjust as needed
-
-            // List to store the transforms of ItemUI instances
-            List<Transform> itemUITransforms = new List<Transform>();
-
             for (int i = 0; i < slotCount; i++)
             {
                 float angle = (startingAngle + i * angleStep) * Mathf.Deg2Rad;
-                float x = Mathf.Sin(angle) * radius;
-                float z = Mathf.Cos(angle) * radius;
-
-                z += radius;
+                float x = Mathf.Sin(angle) * haloScale * 2;
+                float z = Mathf.Cos(angle) * haloScale + haloScale;
 
                 GameObject slot = Instantiate(inventorySlotPrefab, inventoryPanel);
-                inventorySlots.Add(slot);
+                slot.transform.localPosition = new Vector3(x, 0f, z);
 
-                // Set position of inventory slot
-                slot.transform.localPosition = new Vector3(x, 0f, z / 2f);
-
-                InventoryItem inventoryItem = inventorySO.GetItemAt(i);
-
-                // Get the ItemUI component from the inventory slot prefab
+                int invIndex;
+                if (i <= slotCount / 2)
+                {
+                    invIndex = i % inventorySize;
+                }
+                else
+                {
+                    invIndex = (i - slotCount + inventorySize * slotCount) % inventorySize;
+                }
+                InventoryItem inventoryItem = inventorySO.GetItemAt(invIndex);
                 ItemUI itemUI = slot.GetComponentInChildren<ItemUI>();
-                itemUI.SetIndex(i);
-                itemUI.SetActualIndex(i);
-                
-                    // Update the item image
-
-
-                // Store the transform of the ItemUI instance
-                itemUITransforms.Add(itemUI.GetCachedTransform());
-            }
-            for (int i = 0; i <= 8; i++)
-            {
-                InventoryItem inventoryItem = inventorySO.GetItemAt(i);
-                ItemUI itemUI = inventorySlots[i].GetComponentInChildren<ItemUI>();
-                itemUI.SetActualIndex(i);
-                itemUI.UpdateItem(inventoryItem);
-            }
-
-            int reverseIndex = inventorySize - 1; // Start from the last item
-            for (int i = slotCount - 1; i >= 9; i--)
-            {
-                if (reverseIndex < 0)
-                    reverseIndex = inventorySize - 1;
-
-                InventoryItem inventoryItem = inventorySO.GetItemAt(reverseIndex);
-                ItemUI itemUI = inventorySlots[i].GetComponentInChildren<ItemUI>();
-                itemUI.SetActualIndex(reverseIndex);
+                itemUI.SetIndex(invIndex);
                 itemUI.UpdateItem(inventoryItem);
 
-                reverseIndex--; // Move to the previous item
+                inventorySlots.Add(slot);
             }
+            Debug.Log("Initialized slots");
+            LogDumpSlots();
 
-            SortInventorySlotsByZPosition();
+            ArrangeSlotsByZPosition();
             UpdateItemCounter();
         }
-        SortInventorySlotsByZPosition();
-            UpdateItemCounter();
     }
 
-    
     private void UpdateItemCounter()
     {
         if (itemCounterText != null && inventorySO != null)
@@ -238,18 +189,15 @@ public class InventoryUIManager : MonoBehaviour
         }
     }
 
-
     private void ClearInventoryUI()
     {
-        // Call ResetOptions from OptionsPanelManager script if it's available
         OptionsPanelManager optionsPanelManager = FindObjectOfType<OptionsPanelManager>();
         if (optionsPanelManager != null)
         {
             optionsPanelManager.ResetOptions();
-            optionsPanelManager.ToggleOptionsPanel(false); // Toggle options panel off
+            optionsPanelManager.ToggleOptionsPanel(false);
         }
 
-        // Destroy inventory slots
         foreach (GameObject slot in inventorySlots)
         {
             Destroy(slot);
@@ -260,41 +208,6 @@ public class InventoryUIManager : MonoBehaviour
         {
             emptyInventoryImage.SetActive(false);
         }
-
-    }
-    public void UpdateInventoryItems(int direction)
-    {
-        // Find the items in the slots with a Z position approximately equal to 30.86583
-        InventoryItem[] itemsAtZ30_8 = FindItemsAtZPosition(30.86583f);
-
-        // Log the count of items found at Z position 30.86583
-        Debug.Log("Items found at Z position 30.86583 count: " + itemsAtZ30_8.Length);
-
-        // Ensure that itemsAtZ30_8 array is not empty before accessing its elements
-        if (itemsAtZ30_8.Length > 0)
-        {
-            // Get the current item displayed in the slots with Z = 30.86583
-            InventoryItem currentItem = itemsAtZ30_8[0];
-
-            // Find the index of the current item in the inventory
-            int currentIndex = inventorySO.GetCurrentInventoryState().Keys.FirstOrDefault(index => inventorySO.GetCurrentInventoryState()[index] == currentItem);
-
-            // Calculate the index of the next item based on the direction
-            int nextIndex = (currentIndex + direction + inventorySO.Size) % inventorySO.Size;
-
-            // Get the next inventory item to display
-            InventoryItem nextItem = inventorySO.GetItemAt(nextIndex);
-
-            // Update the item image in the UI with the next inventory item
-            foreach (var item in itemsAtZ30_8)
-            {
- 
-            }
-        }
-        else
-        {
-            Debug.LogWarning("No items found at Z position 30.86583");
-        }
     }
 
     private InventoryItem[] FindItemsAtZPosition(float zPosition)
@@ -304,7 +217,7 @@ public class InventoryUIManager : MonoBehaviour
         foreach (var slot in inventorySlots)
         {
             ItemUI itemUI = slot.GetComponentInChildren<ItemUI>();
-            if (itemUI != null && Mathf.Approximately(slot.transform.localPosition.z, zPosition))
+            if (itemUI != null && Mathf.Approximately(itemUI.GetCachedTransform().localPosition.z, zPosition))
             {
                 items.Add(itemUI.inventoryItem);
             }
@@ -313,40 +226,22 @@ public class InventoryUIManager : MonoBehaviour
         return items.ToArray();
     }
 
-
-    private int GetHighestZIndex()
+    private void ArrangeSlotsByZPosition()
     {
-        float highestZ = float.MinValue;
-        int highestZIndex = -1;
-
-        for (int i = 0; i < inventorySlots.Count; i++)
-        {
-            if (inventorySlots[i].transform.localPosition.z > highestZ)
-            {
-                highestZ = inventorySlots[i].transform.localPosition.z;
-                highestZIndex = i;
-            }
-        }
-
-        return highestZIndex;
-    }
-
-    private void SortInventorySlotsByZPosition()
-    {
-        // Sort the inventory slots based on their z-axis position
-        inventorySlots.Sort((a, b) =>
+        List<GameObject> sortedSlots = inventorySlots.ToList();
+        sortedSlots.Sort((a, b) =>
         {
             float zPosA = a.transform.position.z;
             float zPosB = b.transform.position.z;
             return zPosB.CompareTo(zPosA); // Compare in reverse order
         });
 
-        // Reorder the inventory slots based on the sorted list
-        for (int i = 0; i < inventorySlots.Count; i++)
+        for (int i = 0; i < sortedSlots.Count; i++)
         {
-            inventorySlots[i].transform.SetSiblingIndex(i);
+            sortedSlots[i].transform.SetSiblingIndex(i);
         }
     }
+
     private IEnumerator SmoothMoveItems(List<Transform> itemUITransforms, List<Vector3> targetPositions, float duration)
     {
         float elapsedTime = 0f;
@@ -367,8 +262,8 @@ public class InventoryUIManager : MonoBehaviour
                 }
             }
             elapsedTime += Time.deltaTime;
-            SortInventorySlotsByZPosition();
-            
+            ArrangeSlotsByZPosition();
+
             yield return null;
         }
 
@@ -384,6 +279,21 @@ public class InventoryUIManager : MonoBehaviour
         SetSpecs();
     }
 
+    private void LogDumpSlots()
+    {
+        if (!doLogSlots)
+        {
+            return;
+        }
+        int i = 0;
+        Debug.Log("--- slots: ---");
+        foreach (var slot in inventorySlots)
+        {
+            ItemUI itemUI = slot.GetComponentInChildren<ItemUI>();
+            Debug.LogFormat("- i: {0} Z: {1} item: {2} ({3})", i++, slot.transform.localPosition.z, itemUI.GetIndex(), itemUI.inventoryItem.name);
+        }
+    }
+
     private void ShiftInventorySlots(int direction)
     {
         if (isMoving)
@@ -395,20 +305,18 @@ public class InventoryUIManager : MonoBehaviour
 
         List<Transform> itemUITransforms = new List<Transform>();
         List<int> itemUIIndices = new List<int>();
-        foreach (var slot in inventoryPanel.GetComponentsInChildren<ItemUI>())
-        {
-            itemUITransforms.Add(slot.GetCachedTransform());
-            itemUIIndices.Add(slot.GetIndex());
-        }
-
         List<Vector3> initialPositions = new List<Vector3>();
-        foreach (var transform in itemUITransforms)
+        foreach (var slot in inventorySlots)
         {
+            ItemUI itemUI = slot.GetComponentInChildren<ItemUI>();
+            Transform transform = itemUI.GetCachedTransform();
+            itemUITransforms.Add(transform);
             initialPositions.Add(transform.localPosition);
+            itemUIIndices.Add(itemUI.GetIndex());
         }
 
-        int itemCount = inventorySO.Size;
-        int slotCount = itemUITransforms.Count;
+        int inventorySize = inventorySO.Size;
+        int slotCount = inventorySlots.Count;
 
         List<Vector3> newPositions = new List<Vector3>();
         for (int i = 0; i < slotCount; i++)
@@ -416,41 +324,34 @@ public class InventoryUIManager : MonoBehaviour
             Vector3 targetPosition = initialPositions[(i + direction + slotCount) % slotCount];
             newPositions.Add(targetPosition);
 
-            // Update the inventory item at the highest Z position (z = 100)
-            if (Mathf.Approximately(targetPosition.z, 100f))
+            if (Mathf.Approximately(initialPositions[i].z, haloScale * 2))
             {
-                int newIndex = (itemUIIndices[(i + direction + slotCount) % slotCount] - direction + itemCount) % itemCount;
+                int newIndex = (itemUIIndices[(i + direction + slotCount) % slotCount] - direction + inventorySize) % inventorySize;
                 ItemUI targetItemUI = inventorySlots[i].GetComponentInChildren<ItemUI>();
                 InventoryItem newItem = inventorySO.GetItemAt(newIndex);
                 if (targetItemUI != null)
                 {
                     targetItemUI.UpdateItem(newItem);
-                    Debug.LogFormat("Updating slot at Z position 100 to display item at index {0}: {1}", newIndex, newItem.name);
+                    targetItemUI.SetIndex(newIndex);
+                    Debug.LogFormat("Updating slot {0} moving from max Z position to display item at index {1}: {2}", i, newIndex, newItem.name);
                 }
                 else
                 {
-                    Debug.LogWarning("Slot at Z position 100 not found.");
+                    Debug.LogWarning("Slot moving from max Z position not found.");
                 }
             }
         }
+        Debug.Log("Shifted slots " + direction + " ... positions before smooth moving:");
+        LogDumpSlots();
 
         StartCoroutine(SmoothMoveItems(itemUITransforms, newPositions, 0.13f));
-
         StartCoroutine(ResetIsMovingFlag(0.14f));
-
-        int specIndex = (currentIndex + direction + itemCount) % itemCount;
-        if (specIndex < 0)
-        {
-            specIndex = itemCount - 1;
-        }
     }
 
-    // Coroutine to reset the isMoving flag after a delay
     private IEnumerator ResetIsMovingFlag(float delay)
     {
         yield return new WaitForSeconds(delay);
         isMoving = false; // Reset the flag to indicate movement has finished
-        
     }
 
     private void SetSpecs()
@@ -458,7 +359,7 @@ public class InventoryUIManager : MonoBehaviour
         foreach (var slot in inventorySlots)
         {
             ItemUI itemUI = slot.GetComponentInChildren<ItemUI>();
-            if (itemUI != null && Mathf.Approximately(slot.transform.localPosition.z, 0f))
+            if (itemUI != null && Mathf.Approximately(itemUI.GetCachedTransform().localPosition.z, 0f))
             {
                 InventoryItem item = itemUI.inventoryItem;
                 if (item != null && specTXT != null)
